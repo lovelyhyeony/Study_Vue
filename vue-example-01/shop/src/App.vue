10dbc2f4140b7d970b3fe0a56904655901d25d76
lovelyhyeony <dmlgus1222@naver.com>
2021-03-09 23:42:15 +0900

    라우터 적용

---
+++ vue-example-01/shop/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+<script>
+/**
+ * @ : vue cli가 자체적으로
+ * src 디렉토리와 연결해놓은 문자열!
+ *
+ * @/components/Header.vue :
+ * src 아래에 있는
+ * components 디렉토리 아래에 있는
+ * Header.vue 파일을 의미
+ */
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  mounted() {
+    // mounted method에
+    // initialize method를 실행하는 코드를 추가하면
+    // 장바구니 기능이 활성화 됌
+    $.initialize();
+  },
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
