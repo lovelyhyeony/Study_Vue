b509c97e93fac55571d8d7511ede3df00a933ce8
lovelyhyeony <dmlgus1222@naver.com>
2021-03-15 18:54:00 +0900

    vue 입문

--- test/README.md
+++ test/README.md
@@ -1,24 +1,6 @@
-# test
-
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+# 2021-03-15 Vue.js 입문
+- 참고 링크 : [개발자의품격](https://www.youtube.com/watch?v=sqH0u8wN4Rs&list=LL&index=1)
+  
+## Content
+- vue.js 는 컴포넌트(Component)기반 SPA(Single Page Application)을 구현할 수 있도록 도와주는 웹 프론트엔드 프레임워크 중 하나
+- 
---
+++ test/src/views/Home.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <h1>Welcome to {{ title }}!</h1>
+    <input type="text" v-model="input1"/>
+    <button type="button" @click="getData">Get</button>
+    <button type="button" @click="setData">Set</button>
+
+    <select class="form-control" v-model="region" @change="changeRegion">
+      <option :key="i" :value="d.v" v-for="(d,i) in options">{{ d.t }}</option>
+    </select>
+
+    <table class="table table-bordered" v-show="tableShow">
+      <tr :key="i" v-for="(d,i) in options">
+        <td>{{ d.v }}</td>
+        <td>{{ d.t }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: "개발자의 품격",
+      title2: "Seoul",
+      input1: "abcd",
+      options: [
+        {v: "S", t: "Seoul"},
+        {v: "J", t: "Jeju"},
+        {v: "B", t: "Busan"}
+      ],
+      region: "B", // v-model을 통해 (셀렉트 콤보박스가 제일 먼저 부산이 나옴)
+      tableShow: true
+    };
+  },
+  watch: {
+    // 변수명을 function형식으로
+    // 데이터가 변경이되면 바로 캐치해서 알 수 있음
+    /**
+     * 계속적으로 특정 데이터를 계속 관찰하고 있다가
+     * 데이터가 변경이 일어났을때 캐치를 해서 사용할 수 있다.
+     * 우리가 선언한 변수명하고 동일한 메서드명을 만들면 된다.
+     */
+    input1() {
+      console.log(this.input1);
+    }
+  },
+  methods: {
+    getData() {
+      alert(this.input1);
+    },
+    setData() {
+      this.input1 = 12345;
+    },
+    changeRegion() {
+      alert(this.region);
+    }
+  },
+
+  /**
+   * 라이프사이클 훅
+   */
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy");
+  },
+  destroyed() {
+    console.log("destroyed");
+  }
+};
+</script>
